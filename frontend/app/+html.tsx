13c13d59829f8f355aaf52f30c087975b9a99e94
admin <admin@adminui-MacBookPro.local>
2026-01-15 10:55:19 +0900

    Fix: Add notranslate meta tag to prevent automatic browser translation issues

--- frontend/app/+html.tsx
+++ frontend/app/+html.tsx
@@ -15,6 +15,7 @@ export default function Root({ children }: PropsWithChildren) {
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta property="og:locale" content="ko_KR" />
+                <meta name="google" content="notranslate" />
 
                 {/* 
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native. 

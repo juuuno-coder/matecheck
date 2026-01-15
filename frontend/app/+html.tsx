a7227a96c733837f527767226040085f24a087ea
admin <admin@adminui-MacBookPro.local>
2026-01-15 09:58:40 +0900

    Config: Set default web language to Korean (ko)

--- frontend/app/+html.tsx
+++ frontend/app/+html.tsx
@@ -14,6 +14,7 @@ export default function Root({ children }: PropsWithChildren) {
                 <meta charSet="utf-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta property="og:locale" content="ko_KR" />
 
                 {/* 
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native. 
